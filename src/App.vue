803601b1d5f7628f8f7af0b039f50259a7b4c257
2389451262@qq.com <2389451262@qq.com>
2022-07-13 23:19:05 +0800

    chore: remove useless config

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-=======
 <script lang="ts" setup>
-
->>>>>>> 84e4f01 (chore(all): use husky and lint tools)
 </script>
+
 <template>
-<<<<<<< HEAD
-  <img alt="Vue logo"
-      src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-=======
-  <div></div>
->>>>>>> 84e4f01 (chore(all): use husky and lint tools)
+	<div></div>
 </template>
