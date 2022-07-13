89a4b8a7e9c6fcd1f80d2b96fa1dbcebba869ded
2389451262@qq.com <2389451262@qq.com>
2022-07-13 23:33:45 +0800

    chore: clear config

--- .cz-config.js
+++ .cz-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   types: [
+    { value: 'chore', name: 'chore:     构建过程或辅助工具的变动' },
     { value: 'feat', name: 'feat:      新功能' },
     { value: 'fix', name: 'fix:       修复' },
     { value: 'docs', name: 'docs:      文档变更' },
@@ -7,7 +8,6 @@ module.exports = {
     { value: 'refactor', name: 'refactor:  重构' },
     { value: 'pref', name: 'pref:      性能优化' },
     { value: 'test', name: 'test:      增加测试' },
-    { value: 'chore', name: 'chore:     构建过程或辅助工具的变动' },
     { value: 'revert', name: 'revert:    回退' },
     { value: 'build', name: 'build:     打包' }
   ],
--- main.js
+++ main.js
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
-const { app, BrowserWindow } = require('electron')
-=======
 const { app, BrowserWindow } = require('electron');
 
->>>>>>> 84e4f01 (chore(all): use husky and lint tools)
 const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 800,
 		height: 600,
 		webPreferences: {
-<<<<<<< HEAD
-			nodeIntegration: true
-		}
-	})
-	if (process.env.npm_lifecycle_event === 'electron:start') {
-		win.loadURL('http://localhost:3000')
-		win.webContents.openDevTools()
-	} else {
-		win.loadFile('dist/index.html')
-	}
-}
-app.whenReady().then(createWindow)
-=======
 			nodeIntegration: true,
 		},
 	});
@@ -33,8 +16,6 @@ app.whenReady().then(createWindow)
 	}
 };
 app.whenReady().then(createWindow);
->>>>>>> 84e4f01 (chore(all): use husky and lint tools)
-
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
